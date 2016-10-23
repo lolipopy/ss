01cdfb9208e7548fc1d978789010e533a4203ce5
unknown <14301133@bjtu.edu.cn>
2016-10-23 20:38:05 +0800

    no message

---
+++ 14301133/src/index_jsp.java
@@ -0,0 +1,42 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import Servlet.*;
+public class index_jsp implements Servlet{
+@Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+request.setCharacterEncoding("utf-8");
+PrintWriter out = response.getWriter();
+
+
+out.println("<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\" \"http://www.w3.org/TR/html4/loose.dtd\">");
+out.println("<html>");
+out.println("<head>");
+out.println("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\">");
+out.println("<title>跳转到登录页面</title>");
+out.println("</head>");
+out.println("<body>");
+out.println("<p>hello world</p>");
+out.println("</body>");
+out.println("</html>");
+}
+}
